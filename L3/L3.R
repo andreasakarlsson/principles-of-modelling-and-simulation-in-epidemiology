d5df9546671f9145a78c03864b59703ce9b53297
andreasakarlsson <andreas.a.karlsson@gmail.com>
2014-02-08 16:44:38 +0100

    Lab 3 with parallelisation

--- L3/L3.R
+++ L3/L3.R
@@ -16,15 +16,15 @@ require(GillespieSSA) #Gillespie Stochastic Simulation Algorithm with "Explicit 
 ## More info at http://www.jstatsoft.org/v25/i12/paper
 require(ggplot2) #Extravagant plotting tool (not necessary to answer the questions)
 require(plyr) #Handy data manipulation tool (not necessary to answer the questions)
+require(parallel) # For Parallel Computation (not necessary to answer the questions)
+require(data.table) # Similar to a data.frame but faster (not necessary to answer the questions)
 
 ## =============================================================================
 ## 3 Non-infection models
 ## =============================================================================
 ## @knitr Stage_Comp_conf
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = double(), X = double())
-# Should be 1000
-for (i in 1:10){
+mc_ssa <- function(i) {
   init <- c(X = rbinom(n = 1, size = 75, prob =0.15))
   parms <- c(T = 144)
   a <- c("X / T")
@@ -36,32 +36,44 @@ for (i in 1:10){
   colnames(out$data)[1] <- 'Time'
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], X = out$data[,2],
-                                       Model = rep('Stochastic Logistic Model',nrow(out$data))))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], X = out$data[,2], 
+                        Model = rep('Single Compartment',nrow(out$data)))
+  return(rep_out)
 }
-within_summary <- ddply(sim_out, .(Repetition), summarise, Progression = tail(X, n=1) - X[1] )
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
+
+within_summary <- ddply(sim_out, .(Repetition), summarise, Progression = tail(X, n=1))
 percent_prog <- sum(within_summary$Progression!=0)/nrow(within_summary) * 100
 print(percent_prog)
 
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = double(), X = double())
-# Should be 1000
-for (i in 1:1000){
-  init <- c(X1 = rbinom(n = 1, size = 75, prob =0.15),X2 =1, X3 =1, X4 =1)
-  parms <- c(T = 36)
-  a <- c("X1 / T","X2 / T","X3 / T","X4 / T")
-  nu <- matrix(c(+1,0,0,0, 0,+1,0,0, 0,0,+1,0, 0,0,0,+1),nrow=4)
-  out <- ssa(init, a , nu, parms, tf=53, tau = 0.1, method="ETL", maxWallTime=5, simName='Cancer Progression')
+mc_ssa <- function(i) {
+  init <- c(SITU_1 = rbinom(n = 1, size = 75, prob =0.15),SITU_2 =0, SITU_3 =0, SITU_4 =0, CANCER_4 =0)
+  parms <- c(T1 = 36)
+  a <- c("SITU_1 / T1","SITU_2 / T1","SITU_3 / T1","SITU_4 / T1")
+  nu <- matrix(c(-1, 0, 0, 0,
+                 +1,-1, 0, 0,
+                  0,+1,-1, 0,
+                  0, 0,+1,-1,
+                  0, 0, 0,+1), nrow=5, byrow=T)
+  out <- ssa(init, a , nu, parms, tf=53, tau = 0.1, method="ETL", maxWallTime=5, simName='Multiple Compartments')
   #'Explicit tau-leap' => user-defined step size
   if (i ==1)
     ssa.plot(out)
   colnames(out$data)[1] <- 'Time'
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], X = out$data[,5],
-                                       Model = rep('Stochastic Logistic Model',nrow(out$data))))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], X = out$data[,5], 
+                        Model = rep('Multiple Compartments',nrow(out$data)))
+  return(rep_out)
 }
-within_summary <- ddply(sim_out, .(Repetition), summarise, Progression = tail(X, n=1) - X[1] )
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
+
+within_summary <- ddply(sim_out, .(Repetition), summarise, Progression = tail(X, n=1))
 percent_prog <- sum(within_summary$Progression!=0)/nrow(within_summary) * 100
 print(percent_prog)
 
@@ -102,7 +114,7 @@ out <- as.data.frame(ode(y = init, times = times, func = siir, parms = parameter
 out$time <- NULL
 matplot(times, out, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "SIIR Model", lwd = 1, lty = 1, bty = "l", col = 2:5)
 legend("topright", c("Susceptibles", "I1", "I2", "Recovereds"), pch = 1, col = 2:5)
-tail(n=1,out["S"]) + tail(n=1,out["R"])
+
 
 rm(list=ls(all=TRUE))
 siiir <- function(time, state, parameters) {
@@ -123,7 +135,6 @@ out <- as.data.frame(ode(y = init, times = times, func = siiir, parms = paramete
 out$time <- NULL
 matplot(times, out, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "SIIIR Model", lwd = 1, lty = 1, bty = "l", col = 2:6)
 legend("topright", c("Susceptibles", "I1", "I2", "I3", "Recovereds"), pch = 1, col = 2:6)
-tail(n=1,out["S"]) + tail(n=1,out["R"])
 
 rm(list=ls(all=TRUE))
 si5r <- function(time, state, parameters) {
@@ -144,16 +155,13 @@ parameters <- c(r = 1e-4, T1 =12)
 times <- seq(0, 1e3, by = 0.1)
 out <- as.data.frame(ode(y = init, times = times, func = si5r, parms = parameters))
 out$time <- NULL
-matplot(times, out, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "SIIR Model", lwd = 1, lty = 1, bty = "l", col = 2:8)
+matplot(times, out, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "SI5R Model", lwd = 1, lty = 1, bty = "l", col = 2:8)
 legend("topright", c("Susceptibles", "I1", "I2", "I3", "I4", "I5", "Recovereds"), pch = 1, col = 2:8)
-tail(n=1,out["S"]) + tail(n=1,out["R"])
 
 
-## @knitr det_SIR
+## @knitr stoc_SIR
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = numeric(),
-                      S = numeric(), R = numeric(), Model = character())
-for (i in 1:100){ 
+mc_ssa <- function(i) { 
   parms <- c(r=1e-4, T1 = 12)
   init <- c(S = 1000, I = 1, R = 0)
   a <- c("r * S * I", " I / T1")
@@ -162,20 +170,20 @@ for (i in 1:100){
   #'Explicit tau-leap' => user-defined step size
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], S = out$data[,2],R = out$data[,4], 
-                                       Model = rep('SIR',nrow(out$data))))
-  print(tail(n=1,out$data[,2]) + tail(n=1,out$data[,4]))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,4], 
+                        Model = rep('SIR',nrow(out$data)))
+  return(rep_out)
 }
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
 
 ## Plot results
-ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition)) +
-  facet_wrap( ~ Model, ncol=2)
+ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition))
 
-## @knitr det_SIIR
+## @knitr stoc_SIIR
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = numeric(),
-                      S = numeric(), R = numeric(), Model = character()) 
-for (i in 1:1000){ 
+mc_ssa <- function(i) { 
   parms <- c(r=1e-4, T2 = 12 / 2)
   init <- c(S = 1000, I1 = 1, I2 = 0, R = 0)
   a <- c("r * S * (I1 + I2)", " I1 / T2", " I2 / T2" )
@@ -184,21 +192,21 @@ for (i in 1:1000){
   #'Explicit tau-leap' => user-defined step size
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,5], 
-                                       Model = rep('SIR',nrow(out$data))))
-  print(tail(n=1,out$data[,2]) + tail(n=1,out$data[,4]))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,5], 
+                        Model = rep('SIIR',nrow(out$data)))
+  return(rep_out)
 }
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
 
 ## Plot results
-ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition)) +
-  facet_wrap( ~ Model, ncol=2)
+ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition))
 
 
-## @knitr det_SIIIR
+## @knitr stoc_SIIIR
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = numeric(),
-                      S = numeric(), R = numeric(), Model = character())    
-for (i in 1:100){ 
+mc_ssa <- function(i) { 
   parms <- c(r=1e-4, T3 = 12 / 3)
   init <- c(S = 1000, I1 = 1, I2 = 0, I3 = 0, R = 0)
   a <- c("r * S * (I1 + I2 + I3)", " I1 / T3", " I2 / T3", " I3 / T3")
@@ -207,53 +215,85 @@ for (i in 1:100){
   #'Explicit tau-leap' => user-defined step size
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,6], 
-                                       Model = rep('SIR',nrow(out$data))))
-  print(tail(n=1,out$data[,2]) + tail(n=1,out$data[,5]))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,6], 
+                        Model = rep('SIIIR',nrow(out$data)))
+  return(rep_out)
 }
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
 
 ## Plot results
 ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition)) +
   facet_wrap( ~ Model, ncol=2)
 
-
-
-dS <- -r * S * ( I1 + I2 +I3 + I4 + I5)
-dI1 <- -dS - I1 / (T1 / 5)
-dI2 <- (I1 - I2) / (T1 / 5)
-dI3 <- (I2 - I3) / (T1 / 5)
-dI4 <- (I3 - I4) / (T1 / 5)
-dI5 <- (I4 - I5) / (T1 / 5)
-dR <-  I5 / (T1 / 5)  
-## @knitr det_SIIIR
+## @knitr stoc_SI5R
 rm(list=ls(all=TRUE))
-sim_out <- data.frame(Repetition = numeric(), Sample = numeric(), Time = numeric(),
-                      S = numeric(), R = numeric(), Model = character())    
-for (i in 1:100){ 
+mc_ssa <- function(i) {
   parms <- c(r=1e-4, T5 = 12 / 5)
   init <- c(S = 1000, I1 = 1, I2 = 0, I3 = 0, I4 = 0, I5 = 0, R = 0)
   a <- c("r * S * (I1 + I2 + I3 + I4 + I5)", " I1 / T5", " I2 / T5", " I3 / T5"," I4 / T5"," I5 / T5")
   nu <- matrix(c(-1,  0,  0,  0,  0,  0,
                  +1, -1,  0,  0,  0,  0,
-                  0, +1, -1,  0,  0,  0,
-                  0,  0, +1, -1,  0,  0,
-                  0,  0,  0, +1, -1,  0, 
-                  0,  0,  0,  0, +1, -1,
-                  0,  0,  0,  0,  0, +1), nrow=7, byrow=T)
+                 0, +1, -1,  0,  0,  0,
+                 0,  0, +1, -1,  0,  0,
+                 0,  0,  0, +1, -1,  0, 
+                 0,  0,  0,  0, +1, -1,
+                 0,  0,  0,  0,  0, +1), nrow=7, byrow=T)
   out <- ssa(init, a , nu, parms, tf=10e8, tau = 0.1, method="ETL", ignoreNegativeState=TRUE)
-  #'Explicit tau-leap' => user-defined step size
   Repetition <- rep(i,nrow(out$data))
   Sample <- 1:nrow(out$data)
-  sim_out <- rbind(sim_out, data.frame(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,8], 
-                                       Model = rep('SIR',nrow(out$data))))
-  print(tail(n=1,out$data[,2]) + tail(n=1,out$data[,8]))
+  rep_out <- data.table(Repetition, Sample, Time = out$data[,1], S = out$data[,2], R = out$data[,8], 
+                        Model = rep('SI5R',nrow(out$data)))
+  return(rep_out)
 }
+numWorkers <- 3
+system.time(res <- mclapply(1:1000, mc_ssa, mc.cores = numWorkers)) # ~4 x faster than a loop
+system.time(sim_out <- rbindlist(res)) #Same as do.call("rbind",res), but faster
 
 ## Plot results
 ggplot(sim_out, aes(x=Time, y=R, colour=Repetition)) + geom_line(aes(group = Repetition))
+## =============================================================================
 
-## =============================================================================
-## 5.1 Risk estimates in a cohort study
-## =============================================================================
-## @knitr risk_concept
+## @knitr poisson_system
+pos <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dLS <- +r * 1000 - LS / T1
+    dLS1 <- +r * 1000 - LS1 / T2
+    dLS2 <- (LS1 - LS2) / T2
+    dLS3 <- (LS2 - LS3) / T2
+    dLSP1 <- +r * 1000 + (- LSP1 - LSP3) / T3
+    dLSP2 <- (LSP1 - LSP3) / T3
+    dLSP3 <- (LSP1 + LSP2 - LSP3) / T3
+    return(list(c( dLS, dLS1, dLS2, dLS3, dLSP1, dLSP2, dLSP3)))
+  })
+}
+init <- c(LS = 0, LS1 = 0, LS2 = 0, LS3 = 0, LSP1 = 0, LSP2 = 0, LSP3 = 0)
+parameters <- c(r = 0.0025, T1 = 6, T2 = 2, T3 = 3)
+times <- seq(0, 100, by = 1)
+out <- as.data.frame(ode(y = init, times = times, func = pos, parms = parameters))
+out$time <- NULL
+out$LS1 <- NULL
+out$LS2 <- NULL
+out$LSP1 <- NULL
+out$LSP2 <- NULL
+matplot(times, out, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "Poisson Model", lwd = 1, lty = 1, bty = "l", col = 2:4)
+matplot(times, cumsum(out)/times, type = "l", xlab = "Time", ylab = "Susceptibles and Recovereds", main = "Poisson Model", lwd = 1, lty = 1, bty = "l", col = 2:4)
+
+#legend("topright", c("Susceptibles", "I1", "I2", "I3", "Recovereds"), pch = 1, col = 2:6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
